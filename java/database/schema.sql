b8d77e62b9a00726b9afa0ffc0e16d74febdd7f9
Catherine Chleboski <c.chleboski@gmail.com>
2023-04-12 11:33:48 -0400

    edits to Colins push from yesterday. all sql code.

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -1,5 +1,13 @@
-BEGIN TRANSACTION;
+--BEGIN TRANSACTION;
 
+DROP TABLE IF EXISTS rent;
+DROP TABLE IF EXISTS staff_assignments;
+DROP TABLE IF EXISTS assignments;
+DROP TABLE IF EXISTS maintenance_request;
+DROP TABLE IF EXISTS staff;
+DROP TABLE IF EXISTS tenant;
+DROP TABLE IF EXISTS property;
+DROP TABLE IF EXISTS landlord;
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
@@ -61,7 +69,7 @@ CREATE TABLE staff (
 	--CONSTRAINT fk_users_staff FOREIGN KEY (user_id) REFERENCES users (user_id)
 );
 
-CREATE TABLE maintenence_requests (
+CREATE TABLE maintenence_request (
 	request_id SERIAL,
 	status boolean DEFAULT false,
 	date_requested date,
@@ -74,21 +82,21 @@ CREATE TABLE maintenence_requests (
 );
 
 CREATE TABLE assignments (
-	assignment_id SERIAL PRIMARY KEY,
+	assignments_id SERIAL PRIMARY KEY,
 	request_id int,
 	is_assigned boolean DEFAULT false,
 	date_assigned date,
 	property_id int,
-	CONSTRAINT fk_main_req_assignments FOREIGN KEY (request_id) REFERENCES maintenence_requests (request_id),
+	CONSTRAINT fk_main_req_assignments FOREIGN KEY (request_id) REFERENCES maintenence_request (request_id),
 	CONSTRAINT fk_property_assignments FOREIGN KEY (property_id) REFERENCES property (property_id)
 );
 
-CREATE TABLE staff_assignment (
+CREATE TABLE staff_assignments (
 	staff_id int,
-	assignment_id int,
-	CONSTRAINT fk_staff_staffassignment FOREIGN KEY (staff_id) REFERENCES staff (staff_id),
-	CONSTRAINT fk_staffassignments_assignment FOREIGN KEY (assignment_id) REFERENCES assignments (assignment_id),
-	CONSTRAINT pk_staff_staffassignment PRIMARY KEY (staff_id, assignment_id)
+	assignments_id int,
+	CONSTRAINT fk_staff_staffassignments FOREIGN KEY (staff_id) REFERENCES staff (staff_id),
+	CONSTRAINT fk_staffassignments_assignments FOREIGN KEY (assignments_id) REFERENCES assignments (assignments_id),
+	CONSTRAINT pk_staff_staffassignments PRIMARY KEY (staff_id, assignments_id)
 );
 
 CREATE TABLE rent (
@@ -106,7 +114,7 @@ CREATE TABLE rent (
 	CONSTRAINT fk_landlord_rent FOREIGN KEY (landlord_id) REFERENCES landlord (landlord_id)
 
 );
-
+/*
 SELECT * FROM users
 JOIN landlord on landlord.landlord_id = users.user_id;
 SELECT * FROM landlord;
@@ -115,19 +123,10 @@ SELECT * FROM tenant
 JOIN users on tenant.tenant_id = users.user_id;
 SELECT * FROM staff
 JOIN users on staff.staff_id = users.user_id;
-SELECT * FROM maintenence_requests;
+SELECT * FROM maintenence_request;
 SELECT * FROM assignments;
-SELECT * FROM staff_assignment;
+SELECT * FROM staff_assignments;
 SELECT * FROM rent;
+*/
 
-
-
-
-
-
-
-
-ROLLBACK;
-
-
-
+--ROLLBACK;
