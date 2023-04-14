5d5e1c2ed11469cf2333f9ae0f11d7afcc42cb3a
Catherine Chleboski <c.chleboski@gmail.com>
2023-04-14 16:09:42 -0400

    made some empty files and added to StaffAssignment interface and JdbcDao

---
+++ java/src/main/java/com/techelevator/dao/JdbcStaffAssignmentDao.java
@@ -0,0 +1,33 @@
+package com.techelevator.dao;
+
+import com.techelevator.model.StaffAssignment;
+
+import java.util.List;
+
+public class JdbcStaffAssignmentDao {
+
+    public boolean createStaffAssignment(int staffId, int assignmentId) {
+
+
+        return true;
+    }
+
+    public StaffAssignment getStaffAssignment(int staffId, int assignmentId) {
+
+
+        return null;
+    }
+
+    public List<StaffAssignment> getStaffAssignmentsByStaffId(int staffId) {
+
+
+        return null;
+    }
+
+    public List<StaffAssignment> getStaffAssignmentsByAssignmentId(int assignmentId) {
+
+
+        return null;
+    }
+
+}
