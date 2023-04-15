45a62217a5810a2fea779f77dd4d83abb8d82b03
[A[BRyan Babirad <ryanbabirad@gmail.com>
2023-04-15 13:44:26 -0400

    Saving progress so I can check out Josie

--- vue/src/components/TheHeader.vue
+++ vue/src/components/TheHeader.vue
@@ -2,14 +2,12 @@
   <section class="nav">
     <ul>
       <li>
-        <router-link class="links router-link-active" v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;
-        <router-link class="links router-link-active" v-if="$store.state.token === ''" v-bind:to="{ name: 'login' }">Login</router-link>&nbsp;
-        <router-link class="links router-link-active" v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>&nbsp;
-        <router-link class="links router-link-active" v-bind:to="{ name: 'landlord' }" v-if="$store.state.role = 'landlord'">Landlord</router-link>&nbsp;
-        <router-link class="links router-link-active" v-bind:to="{ name: 'staff' }" v-if="$store.state.role= 'staff'">Staff</router-link>
-        <router-link class="links router-link-active" v-bind:to="{ name: 'home' }" tag="button">Home</router-link>&nbsp;
+        <router-link class="links router-link-active" v-bind:to="{ name: 'home' }" tag="button">Home</router-link>
         <router-link class="links router-link-active" v-if="$store.state.token === ''" v-bind:to="{ name: 'login' }" tag="button">Login</router-link>&nbsp;
-        <router-link class="links router-link-active" v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''" tag="button">Logout</router-link>
+        <router-link class="links router-link-active" v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''" tag="button">Logout</router-link>&nbsp;
+        <router-link class="links router-link-active" v-bind:to="{ name: 'landlord' }" v-if="$store.state.role = 'landlord'">Landlord</router-link>&nbsp;
+        <router-link class="links router-link-active" v-bind:to="{ name: 'staff' }" v-if="$store.state.role= 'staff'">Staff</router-link>&nbsp;
+        <router-link class="links router-link-active" v-bind:to="{ name: 'tenant' }" v-if="$store.state.role= 'tenant'">Tenant</router-link>
       </li>
     </ul>
   </section>
