949af726f5ef5c6517aac19530adae1e8b1be54e
[A[BRyan Babirad <ryanbabirad@gmail.com>
2023-04-15 16:45:36 -0400

    Latest on the front end. Header stuff

--- vue/src/components/TheHeader.vue
+++ vue/src/components/TheHeader.vue
@@ -2,34 +2,43 @@
   <section class="nav">
     <ul>
       <li>
-        <router-link class="links router-link-active" v-bind:to="{ name: 'home' }" >Home</router-link>&nbsp;
-        <router-link class="links router-link-active" v-bind:to="{ name: 'login' }"  v-if="!isLoggedIn">Login</router-link>&nbsp;
+        <router-link class="links router-link-active" v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;
+        <router-link class="links router-link-active" v-bind:to="{ name: 'login' }" v-if="!isLoggedIn">Login</router-link>&nbsp;
         <router-link class="links router-link-active" v-bind:to="{ name: 'logout' }" v-if="isLoggedIn">Logout</router-link>&nbsp;
-        <router-link class="links router-link-active" v-bind:to="{ name: 'staff' }"  v-if="isStaff">Staff</router-link>&nbsp;
+        <router-link class="links router-link-active" v-bind:to="{ name: 'landlord' }" v-if="isRole">landlord</router-link>&nbsp;
       </li>
     </ul>
   </section>
 </template>
 
 <script>
-
 export default {
-    name: "header",
-    computed: {
-      isLandlord() {
-        return this.$store.state.user.authorities[0].name === 'ROLE_LANDLORD';
-      },
-      isStaff(){
-        return this.$store.state.user.authorities[0].name === 'ROLE_STAFF';
-      },
-      isTenant() {
-        return this.$store.state.user.authorities[0].name === 'ROLE_TENANT';
-      },
-      isLoggedIn() {
-        return this.$store.state.token !== '';
-      }
-    }
+  name: "header",
+  computed: {
+    isRole() {
+      if (
+        !this.$store.state.user ||
+        !this.$store.state.user.authorities ||
+        this.$store.state.user.authorities.length === 0
+      ) {
+        return false;
+      } 
 
+      if (this.$store.state.user.authorities[0].name === "ROLE_LANDLORD") {
+      return this.$store.state.user.authorities[0].name === "ROLE_LANDLORD";
+      }
+      if (this.$store.state.user.authorities[0].name === "ROLE_STAFF") {
+      return this.$store.state.user.authorities[0].name === "ROLE_STAFF";
+      }
+      if (this.$store.state.user.authorities[0].name === "ROLE_TENANT") {
+      return this.$store.state.user.authorities[0].name === "ROLE_TENANT";
+      }
+      return this.$store.state.user.authorities[0].name
+    },
+    isLoggedIn() {
+      return this.$store.state.token !== "";
+    }
+  },
 };
 </script>
 
@@ -43,27 +52,23 @@ export default {
   left: 0;
   right: 0;
   background-color: #006e7f;
-  
 }
-.links{
-    margin:5px;
-    padding-left: 10px;
-    padding-right: 10px;
-    color:black;
-    border-radius: 8px;
-    background-color:white;
-    border: 1px solid black;
-    font-family:monospace;
-    font-weight:bold;
-    letter-spacing:1px;
-
+.links {
+  margin: 5px;
+  padding-left: 10px;
+  padding-right: 10px;
+  color: black;
+  border-radius: 8px;
+  background-color: white;
+  border: 1px solid black;
+  font-family: monospace;
+  font-weight: bold;
+  letter-spacing: 1px;
 }
-.links:hover{
-    color:white;
-    background-color:#F8CB2E ;
-    border: 1px solid #F8CB2E;
-    cursor:pointer;
+.links:hover {
+  color: white;
+  background-color: #f8cb2e;
+  border: 1px solid #f8cb2e;
+  cursor: pointer;
 }
-
-
 </style>
