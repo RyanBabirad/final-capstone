2527531460026525e98783add346e692f41fe727
Catherine Chleboski <c.chleboski@gmail.com>
2023-04-18 19:14:31 -0400

    finally merging the new property code

--- java/src/main/java/com/techelevator/dao/JdbcRentDao.java
+++ java/src/main/java/com/techelevator/dao/JdbcRentDao.java
@@ -1,11 +1,15 @@
 package com.techelevator.dao;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Component;
 import com.techelevator.model.Rent;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Component
-public class JdbcRentDao {
+public class JdbcRentDao implements RentDao {
 
     // instance variables ..............................................................................................
 
@@ -31,5 +35,38 @@ public class JdbcRentDao {
         return numRowsAffected == 1;
     }
 
-    public
+    @Override
+    public List<Rent> getRentsByTenantId(int tenantId) {
+        List<Rent> list = new ArrayList<>();
+
+        String sql = "SELECT * FROM rent WHERE tenant_id = ?";
+        SqlRowSet results = jdbcTemplate.queryForRowSet(sql, tenantId);
+        if (results.next()) {
+            list.add(mapRowToTenant(results));
+        }
+
+        return null;
+    }
+
+    @Override
+    public List<Rent> getRentsByLandlordId(int landlordId) {
+        return null;
+    }
+
+    private Rent mapRowToTenant(SqlRowSet row) {
+        Rent rent = new Rent();
+
+        rent.setRentId(row.getInt("rent_id"));
+        rent.setDueDate(row.getString("duedate"));
+        rent.setPaidDate(row.getString("paiddate"));
+        rent.setPaymentStatus(row.getInt("paymentstatus"));
+        rent.setAmountPaid(row.getFloat("amountpaid"));
+        rent.setAmountDue(row.getFloat("amountdue"));
+        rent.setPropertyId(row.getInt("property_id"));
+        rent.setTenantId(row.getInt("tenant_id"));
+        rent.setLandlordId(row.getInt("landlord_id"));
+
+        return rent;
+    }
+
 }
